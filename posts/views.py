c4d1006472d92878645c628dca9b378389d27b98
eduardo <eduardo@uakami.com>
2016-09-05 23:30:47 -0400

    tryDjango cap 17

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post
@@ -8,7 +8,14 @@ from .models import Post
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    instance = get_object_or_404(Post, id=2)
+    context = {
+        'instance': instance,
+        'title': 'Detail'
+    }
+    return render(request, 'post_detail.html', context)
+    # return HttpResponse("<h1>Detail</h1>")
+
 def post_list(request):
     queryset = Post.objects.all()
     context = {
@@ -27,6 +34,7 @@ def post_list(request):
 
     return render(request, 'index.html', context)
     #return HttpResponse("<h1>List</h1>")
+
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
 def post_delete(request):
