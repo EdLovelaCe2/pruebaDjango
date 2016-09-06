7621874d0e5480f4232c0b60101de92404e7f6f5
eduardo laptope <eduardo@uakami.com.ve>
2016-09-06 12:26:56 -0400

    editar elementos y agregarlos en la base de datos. Tambien mostrarlos.

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post
+from .form import PostForm
 
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        print form.cleaned_data.get('title')
+        instance.save()
+        # mensaje de aprobacion
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    # if request.method == 'POST':
+        # print request.POST.get('title')
+        # print request.POST.get('content')
+        # title = request.POST.get('title')
+        # Post.objects.create(title=title)
+    context = {
+        'form': form,
+    }
+    return render(request, 'post_form.html', context)
 
 def post_detail(request, id=None):
     instance = get_object_or_404(Post, id=id)
@@ -36,7 +53,21 @@ def post_list(request):
     return render(request, 'index.html', context)
     #return HttpResponse("<h1>List</h1>")
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        # mensaje de aprobacion
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        'instance': instance,
+        'title': instance.title,
+        'form': form,
+    }
+    return render(request, 'post_form.html', context)
+
 def post_delete(request):
     return HttpResponse("<h1>Delete</h1>")
