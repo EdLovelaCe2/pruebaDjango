cbc34c87c521dbbe20a577b28e08a2d6325a5fb9
eduardo laptope <eduardo@uakami.com.ve>
2016-09-06 16:45:47 -0400

    itinerancia de los templates, probando como se comunican entre ellos

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from .models import Post
@@ -10,26 +11,23 @@ def post_create(request):
     form = PostForm(request.POST or None)
     if form.is_valid():
         instance = form.save(commit=False)
-        print form.cleaned_data.get('title')
         instance.save()
-        # mensaje de aprobacion
+        # message success
+        messages.success(request, "Successfully Created")
         return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+		messages.error(request, "Not Successfully Created")
 
-    # if request.method == 'POST':
-        # print request.POST.get('title')
-        # print request.POST.get('content')
-        # title = request.POST.get('title')
-        # Post.objects.create(title=title)
     context = {
         'form': form,
-    }
+        }
     return render(request, 'post_form.html', context)
 
 def post_detail(request, id=None):
     instance = get_object_or_404(Post, id=id)
     context = {
         'instance': instance,
-        'title': 'Detail'
+        'title': instance.title,
     }
     return render(request, 'post_detail.html', context)
     # return HttpResponse("<h1>Detail</h1>")
@@ -41,16 +39,7 @@ def post_list(request):
         'title': 'List'
     }
 
-    # if request.user.is_authenticated():
-    #     context = {
-    #         'title': 'My user List'
-    #         }
-    # else:
-    #     context = {
-    #         'title': 'List'
-    #         }
-
-    return render(request, 'index.html', context)
+    return render(request, 'post_list.html', context)
     #return HttpResponse("<h1>List</h1>")
 
 def post_update(request, id=None):
@@ -59,7 +48,7 @@ def post_update(request, id=None):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
-        # mensaje de aprobacion
+        messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
         return HttpResponseRedirect(instance.get_absolute_url())
 
     context = {
@@ -69,5 +58,8 @@ def post_update(request, id=None):
     }
     return render(request, 'post_form.html', context)
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete
+    messages.success(request, "Successfully Deleted")
+    return redirect('posts:list')
